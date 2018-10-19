23026426e5a958d15784e10d74a9f86b6632b878
Sebastian Korotkiewicz <sebastian@korotkiewicz.eu>
2018-10-19 18:29:13 +0200

    small changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 'use strict';
 
-//npm install gulp gulp-minify-css gulp-clean gulp-cleanhtml gulp-uglify-es --save-dev
+//npm install gulp gulp-minify-css gulp-clean gulp-cleanhtml gulp-uglify-es gulp-zip run-sequence --save-dev
 
 var gulp = require('gulp'),
 	clean = require('gulp-clean'),
 	minifycss = require('gulp-minify-css'),
 	uglify = require('gulp-uglify-es').default,
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	zip = require('gulp-zip'),
+	runSequence = require('run-sequence');
 
 //clean assets directory
 gulp.task('clean', function() {
-	return gulp.src('assets/*', {read: false})
+	return gulp.src(['assets/*', 'dist/*'], {read: false})
 		.pipe(clean());
 });
 
@@ -29,7 +31,7 @@ gulp.task('optimize-libs-js', function() {
 });
 
 gulp.task('optimize-css', function() {
-			return gulp.src('static/css/*.css')
+	return gulp.src('static/css/*.css')
 		.pipe(concat('app.css'))
 		.pipe(minifycss({keepSpecialComments: 0}))
 		.pipe(gulp.dest('assets/css'))
@@ -40,8 +42,37 @@ gulp.task('copy', function() {
 		.pipe(gulp.dest('assets/img'));
 });
 
-gulp.task('optimize', ['optimize-app-js', 'optimize-libs-js', 'optimize-css', 'copy']);
+gulp.task('build', function() {
+		gulp.src('assets/**')
+		.pipe(gulp.dest('dist/assets'));
+
+		gulp.src(['templates/*.html'])
+		.pipe(gulp.dest('dist/templates'));
+
+	return gulp.src(['pastes/', 'config.json', 'gbin'])
+		.pipe(gulp.dest('dist'));
+});
+
+/*gulp.task('release', ['optimize-app-js', 'optimize-libs-js', 'optimize-css', 'copy', 'build'], function() {
+		var config = require('./config.json'),
+		distFileName = 'gbin_v' + config.Version + '.zip';
+	
+	return gulp.src('dist/**')
+		.pipe(zip(distFileName))
+		.pipe(gulp.dest('release'));
+});*/
+
+gulp.task('release', function() {
+    runSequence('optimize-app-js', 'optimize-libs-js', 'optimize-css', 'copy', 'build', function() {
+		var config = require('./config.json'),
+		distFileName = 'gbin_v' + config.Version + '.zip';
+	
+		return gulp.src('dist/**')
+			.pipe(zip(distFileName))
+			.pipe(gulp.dest('release'));
+	});
+});
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('optimize');
+	gulp.start('release');
 });
